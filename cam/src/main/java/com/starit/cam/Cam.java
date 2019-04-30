154eab411a7094db18c78106b7c51d4165508a6f
kayhan92 <kayhan.starit@gmail.com>
2019-04-30 17:25:19 +0600

    Initial commit

---
+++ cam/src/main/java/com/starit/cam/Cam.java
@@ -0,0 +1,20 @@
+package com.starit.cam;
+
+public class Cam {
+    String msg = "Hello World";
+
+    public Cam() {
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Cam(String msg) {
+        this.msg = msg;
+    }
+}
